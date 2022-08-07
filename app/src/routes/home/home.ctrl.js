0fdbd48199a5312201b8d4462927ceeab17cd71c
BaeInHoo <qodlsgh0210@naver.com>
2022-08-07 20:22:54 +0900

    백엔드 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict";
 
-const hello = (req, res) => {
-  res.render('home/index');
-};
+const output = {
+  hello : (req, res) => {
+    res.render('home/index');
+  },
+  login : (req, res) => {
+    res.render('home/login');
+  }
+}
 
-const login = (req, res) => {
-  res.render('home/login');
-};
+const users = {
+  id: ["node265", "node266", "node267"],
+  psword: ["123", "1234", "12345"]
+}
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if(users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if(users.psword[idx] === psword) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "login fail",
+    });
+  }
+}
 
 module.exports = {
-  hello,
-  login
+  output,
+  process
 };

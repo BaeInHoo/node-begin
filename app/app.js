099c5019c4c6ed332a272d6ef88708c87dfd4554
BaeInHoo <qodlsgh0210@naver.com>
2022-08-12 21:11:44 +0900

    dotenv로 환경변수 등록

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 "use strict";
 
+// 모듈
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 
 // 라우팅
